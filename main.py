4016869ad04811a6fc8da284fdfc2116ddd32cd5
gabripo <gabripo93+github@gmail.com>
2025-01-17 19:20:04 +0100

    introduce analysis of the screen

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
 from src.chat_handler.Query import QueryHistory, QueryCommander, QueryHandler
+from src.screenshots_manager.Screenshot import ScreenshotManager
 
 model_name = 'llama3.2-vision'
+ANALYZE_SCREEN = True
+ANALYZE_ASSETS = True
 
 if __name__ == "__main__":
     history = QueryHistory()
@@ -10,12 +13,21 @@ if __name__ == "__main__":
     image_query = QueryCommander(query_history=history)
     handler.add_handler(image_query)
     
-    queries = [
-        {'images': [os.path.join(os.getcwd(), 'assets', 'horsehead_nebula.jpg')], 'query': 'What is in this image?'},
-        {'images': [os.path.join(os.getcwd(), 'assets', 'Crab_MultiChandra_960.jpg')], 'query': 'What is in this image?'},
-        {'images': [], 'query': 'Are the objects in the images in the same galaxy cluster?'}
-    ]
-    for query in queries:
-        handler.handle(query)
-    
-    print(history.get_history())
+    if ANALYZE_SCREEN:
+        screenshot_manager = ScreenshotManager()
+        screenshot_manager.set_scale_factor(0.3)
+        screenshot_manager.capture_screenshot()
+        handler.handle({'images': screenshot_manager.screenshots, 'query': 'What do you see on my screen?'})
+        print(history.get_history())
+        history.clear_history()
+
+    if ANALYZE_ASSETS:
+        queries = [
+            {'images': [os.path.join(os.getcwd(), 'assets', 'horsehead_nebula.jpg')], 'query': 'What is in this image?'},
+            {'images': [os.path.join(os.getcwd(), 'assets', 'Crab_MultiChandra_960.jpg')], 'query': 'What is in this image?'},
+            {'images': [], 'query': 'Are the objects in the images in the same galaxy cluster?'}
+        ]
+        for query in queries:
+            handler.handle(query)
+        print(history.get_history())
+        history.clear_history()
