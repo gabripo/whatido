48e59f8489a3384c2210aafc7bd78b1de081cade
gabripo <gabripo93+github@gmail.com>
2025-01-25 12:46:21 +0100

    fix typos in queries

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ if __name__ == "__main__":
     queries = [
         QueryScorePair('write an aggressive e-mail, max 100 characters', [10, 10, 0, 10, 0]),
         QueryScorePair('write an e-mail explaining low-pass filtering, max 300 characters', [10, 3, 10, 0, 2]),
-        QueryScorePair('write an e-mail explaining the usage of QueryScorePair(lant\' in French with an example, max 300 characters', [6, 10, 0, 0, 7]),
-        QueryScorePair('scrivi una e-mail scusandoti di un evento grave QueryScorePair(n azienda, max 200 caratteri e in Italiano', [0, 9, 0, 0, 10]),
+        QueryScorePair('write an e-mail explaining the usage of epoustoflant in French with an example, max 300 characters', [6, 10, 0, 0, 7]),
+        QueryScorePair('scrivi una e-mail scusandoti di un evento grave accaduto in azienda, max 200 caratteri e in Italiano', [0, 9, 0, 0, 10]),
         QueryScorePair('schreib eine aggressive E-Mail, max 200 Buchstaben und auf Deutsch',[0, 10, 0, 10, 0]),
         ]
     de.build(queries, 10)
