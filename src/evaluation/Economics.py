ba4e7b5b133318d0605ab824dad89433112c4e9f
gabripo <gabripo93+github@gmail.com>
2025-01-30 14:59:26 +0100

    default productivity_scores are nothing

--- src/evaluation/Economics.py
+++ src/evaluation/Economics.py
@@ -6,9 +6,9 @@ class Employee:
     def __init__(self, employee_name: str):
         self._name = employee_name
 
-    def compute_profitability(self, productivity_scores: list[float]) -> None:
+    def compute_profitability(self, productivity_scores: list[float] = []) -> None:
         # TODO implement
-        self._productivity_scores = [1.0, 2.0]
+        self._productivity_scores = productivity_scores
         self._profitability = 1.0
         print(f"Employee {self._name} having scores {self._productivity_scores} has profitability {self._profitability}")
         pass
