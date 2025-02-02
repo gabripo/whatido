649b60ee91e1dc866ba60235fd37e4b9907ebb8c
gabripo <gabripo93+github@gmail.com>
2025-02-02 23:48:13 +0100

    enable inference after model loading

--- main.py
+++ main.py
@@ -25,3 +25,5 @@ if __name__ == "__main__":
             base_model_name="unsloth/llama-3.2-11b-vision-instruct-unsloth-bnb-4bit",
             )
         model.get_hf_model(local_save=True)
+    model.enable_inference()
+    # TODO implement inference pipeline
